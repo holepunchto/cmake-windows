368eb19c6c615dbaec53a18e5c31475a00386040
Kasper Isager <kasperisager@hey.com>
2024-04-11 09:29:57 +0200

    Add `find_windows_sdk()`

--- windows.cmake
+++ windows.cmake
@@ -1,7 +1,35 @@
+function(find_windows_sdk)
+  set(one_value_keywords
+    BIN
+  )
+
+  cmake_parse_arguments(
+    PARSE_ARGV 0 ARGV "" "${one_value_keywords}" ""
+  )
+
+  get_filename_component(windows_kits_dir "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows Kits\\Installed Roots;KitsRoot10]" ABSOLUTE)
+
+  set(sdk_version "${CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION}")
+
+  set(sdk_platform "${CMAKE_GENERATOR_PLATFORM}")
+
+  if(DEFINED ARGV_BIN)
+    cmake_path(APPEND windows_kits_dir bin ${sdk_version} ${sdk_platform} OUTPUT_VARIABLE ${ARGV_BIN})
+
+    list(APPEND result ${ARGV_BIN})
+  endif()
+
+  return(PROPAGATE ${result})
+endfunction()
+
 function(find_sign_tool result)
+  find_windows_sdk(BIN sdk_bin_dir)
+
   find_program(
     sign_tool
     NAMES SignTool
+    PATHS "${sdk_bin_dir}"
+    NO_DEFAULT_PATH
     REQUIRED
   )
 
